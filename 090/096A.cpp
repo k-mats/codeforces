21460a450f43e21722061c957eae83cbe8297040
k-mats <gfeenhaftsylphid@gmail.com>
2012-02-12 15:54:27 +0900

    accepted codes from #90 to #99

---
+++ 090/096A.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <limits>
+#include <algorithm>
+#include <utility>
+#include <cstdlib>
+#include <cmath>
+
+// using
+using namespace std;
+
+// typedef
+typedef vector<int> VI;
+typedef vector<VI> VII;
+typedef vector<string> VS;
+typedef vector<long> VL;
+typedef long long LL;
+typedef vector<LL> VLL;
+
+// container utils
+#define PB push_back
+#define PF push_front
+#define GRT(x) greater<(x)>()
+#define ASORT(x) sort((x).begin(), (x).end())
+#define DSORT(x, y) sort((x).begin(), (x).end(), greater<(y)>())
+#define FILL(x, y) fill((x).begin(), (x).end(), (y))
+#define COPY(x, y) (y).clear(); \
+  copy((x).begin(), (x).end(), back_inserter(y))
+
+// repetition
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define FORL(i, a, b) for (long i = (a); i < (b); i++)
+#define REP(i, n) FOR(i, 0, n)
+#define REPL(i, n) FORL(i, 0, n)
+
+// debug
+#define DUMP(a) cerr << #a << " = " << (a) << endl
+#define DUMP2(a, b) cerr << #a << " = " << (a) << ", " << #b << " = " << (b) << endl
+#define DUMP3(a, b, c) cerr << #a << " = " << (a) << ", " << #b << " = " << (b) << ", " << #c << " = " << (c) << endl
+
+void solve() {
+  string str;
+  int cnt = 0;
+  bool one, prev_one;
+
+  cin >> str;
+
+  if (str[0] == '1') one = true;
+  else one = false;
+
+  prev_one = one;
+  cnt = 1;
+
+  FOR(i, 1, str.size()) {
+    if (str[i] == '1') one = true;
+    else one = false;
+    if (one == prev_one) cnt++;
+    else cnt = 1;
+    prev_one = one;
+
+    if (cnt == 7) {
+      cout << "YES" << endl;
+      return;
+    }
+  }
+
+  cout << "NO" << endl;
+}
+	
+
+int main(int argc, char *argv[]) {
+  solve();
+
+  return 0;
+}
